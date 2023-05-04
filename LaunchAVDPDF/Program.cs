ae4bebbe64596eabe7a49c509ce5ce427a069e72
Tim McFadden <timothymcfadden@scom2k7.com>
2023-05-04 10:53:49 -0400

    Initial

---
+++ LaunchAVDPDF/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Security.Principal;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LaunchAVDPDF
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+                string destinationDirectory = ConfigurationManager.AppSettings["destinationDirectory"];
+
+                if (!Directory.Exists(destinationDirectory))
+                {
+                    Console.WriteLine("The specified directory does not exist.");
+                    return;
+                }
+
+                // Get all PDF files in the directory and order them by the last write time (descending)
+                var pdfFiles = Directory.GetFiles(destinationDirectory, "*.pdf")
+                    .Select(f => new FileInfo(f))
+                    .OrderByDescending(fi => fi.LastWriteTime)
+                    .ToList();
+
+                if (pdfFiles.Count == 0)
+                {
+                    Console.WriteLine("No PDF files were found in the specified directory.");
+                    return;
+                }
+
+                // Get the latest PDF file
+                FileInfo latestPdf = pdfFiles.First();
+
+                try
+                {
+                    // Open the PDF file with the default PDF viewer
+                    Process.Start(new ProcessStartInfo(latestPdf.FullName) { UseShellExecute = true });
+                    Console.WriteLine($"Opened the latest PDF file: {latestPdf.FullName}");
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"Error opening the PDF file: {ex.Message}");
+                }
+
+        }
+    }
+}
---
+++ LaunchPDF/Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Security.Principal;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+
+namespace LaunchPDF
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                if (args.Length == 0)
+                {
+                    Console.WriteLine("No file path provided.");
+                    return;
+                }
+
+                string sourceFilePath = args[0];
+                string destinationDirectory = ConfigurationManager.AppSettings["destinationDirectory"];
+
+                if (!File.Exists(sourceFilePath))
+                {
+                    Console.WriteLine($"File not found: {sourceFilePath}");
+                    return;
+                }
+
+                if (!Directory.Exists(destinationDirectory))
+                {
+                    Directory.CreateDirectory(destinationDirectory);
+                }
+                else
+                {
+                    // Delete all existing files in the destination directory
+                    foreach (string file in Directory.GetFiles(destinationDirectory))
+                    {
+                        File.Delete(file);
+                    }
+                }
+
+                string fileName = Path.GetFileName(sourceFilePath);
+                string destinationFilePath = Path.Combine(destinationDirectory, fileName);
+
+                File.Copy(sourceFilePath, destinationFilePath, true);
+
+                Console.WriteLine($"File copied to: {destinationFilePath}");
+
+                // Check if the PDF file exists
+                if (File.Exists(destinationFilePath))
+                {
+                    // Launch the PDF file using the default PDF viewer
+                    //Process.Start(new ProcessStartInfo(destinationFilePath) { UseShellExecute = true });
+                    Console.WriteLine("Launching URI");
+
+                    string workspaceId = ConfigurationManager.AppSettings["workspaceId"];
+                    string resourceId = ConfigurationManager.AppSettings["resourceId"];
+                    string username = ConfigurationManager.AppSettings["username"];
+
+                    string uri = $"ms-avd:connect?workspaceId={workspaceId}&resourceid={resourceId}&username={username}&version=0";
+
+                    try
+                    {
+                        Process.Start(new ProcessStartInfo
+                        {
+                            FileName = uri,
+                            UseShellExecute = true // Use the operating system shell to start the process
+                        });
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine($"Failed to open the URI: {ex.Message}");
+                    }
+
+                }
+                else
+                {
+                    Console.WriteLine($"PDF file not found at: {destinationFilePath}");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error: {ex.Message}");
+            }
+
+        }
+    }
+}
